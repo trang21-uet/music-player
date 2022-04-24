195863c38a70ea5ba85b7b2a9f3ed88be8607c37
Hai Son <nguyenhaison0704@gmail.com>
2022-04-24 21:40:14 +0700

    fixbug url

--- backend/local-storage-service/src/main/java/me/application/music/local/service/LocalFileStorageServiceImpl.java
+++ backend/local-storage-service/src/main/java/me/application/music/local/service/LocalFileStorageServiceImpl.java
@@ -2,6 +2,10 @@ package me.application.music.local.service;
 
 import lombok.SneakyThrows;
 import me.application.music.local.model.FileInfo;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.mp3.Mp3Parser;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
@@ -9,8 +13,13 @@ import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -77,7 +86,26 @@ public class LocalFileStorageServiceImpl implements ILocalFileStorageService {
     public List<FileInfo> getFileInfos(UriComponentsBuilder methodUrl) {
         return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(path -> {
             String fileName = path.getFileName().toString();
-            String url = methodUrl.build(fileName).toString();
+            String url = methodUrl.uri(path.toUri()).build().toString();
+
+
+            try {
+                InputStream input = new FileInputStream(path.toFile());
+                ContentHandler handler = new DefaultHandler();
+                Metadata metadata = new Metadata();
+                Parser parser = new Mp3Parser();
+                ParseContext parseCtx = new ParseContext();
+                parser.parse(input, handler, metadata, parseCtx);
+                input.close();
+
+                List<String> metadataNames = List.of(metadata.names());
+                metadataNames.forEach(name -> {
+                    System.out.println(name + " : " + metadata.get(name));
+                });
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
 
             FileInfo info = new FileInfo();
             info.setFileName(fileName);
