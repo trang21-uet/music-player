7813c9066389d9d2edfc3d59e4a161b744402d5e
Hai Son <nguyenhaison0704@gmail.com>
2022-04-27 14:27:28 +0700

    fixbug url songs

--- backend/cloud-storage-service/src/main/java/me/application/music/service/UserService.java
+++ backend/cloud-storage-service/src/main/java/me/application/music/service/UserService.java
@@ -28,19 +28,19 @@ public class UserService {
         FileInputStream serviceAccount =
                 new FileInputStream("music-application-7bed9-firebase-adminsdk-i8i14-1fdd340eb4.json");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket(bucketName)
-                .build();
-        FirebaseApp.initializeApp(options);
-
-        Bucket bucket = StorageClient.getInstance().bucket();
-
-        BlobId blobId = BlobId.of(bucketName, fileName);
-
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
-                .setContentType("audio/mpeg")
-                .build();
-        bucket.create(String.valueOf(blobInfo), Files.readAllBytes(Paths.get(fileName)));
+//        FirebaseOptions options = FirebaseOptions.builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .setStorageBucket(bucketName)
+//                .build();
+//        FirebaseApp.initializeApp(options);
+//
+//        Bucket bucket = StorageClient.getInstance().bucket();
+//
+//        BlobId blobId = BlobId.of(bucketName, fileName);
+//
+//        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+//                .setContentType("audio/mpeg")
+//                .build();
+//        bucket.create(String.valueOf(blobInfo), Files.readAllBytes(Paths.get(fileName)));
     }
 }
