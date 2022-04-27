62c859a18c59ef0ef8259e31e2a0ef7f79f8921b
Hai Son <nguyenhaison0704@gmail.com>
2022-04-27 14:30:17 +0700

    fixbug url songs

--- backend/cloud-storage-service/src/main/java/me/application/music/service/UserService.java
+++ backend/cloud-storage-service/src/main/java/me/application/music/service/UserService.java
@@ -1,19 +1,9 @@
 package me.application.music.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Bucket;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.StorageClient;
 import lombok.SneakyThrows;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @Service
 public class UserService {
